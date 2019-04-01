b9829b87ca9b5523b03d8eb8bde759ff29704cd5
kristine <kristinek@github.com>
2019-04-01 16:08:39 +0300

    sample 3 and 5 test

--- src/selenium/sample/Sample3Task.java
+++ src/selenium/sample/Sample3Task.java
@@ -36,37 +36,33 @@ public class Sample3Task {
 
     @Test
     public void assertEqualsTask() throws Exception {
-        assertEquals(5, driver.findElements(By.className("test")).size());
-        assertEquals("This is also a button", driver.findElement(By.id("buttonId")).getAttribute("value"));
-//         TODO:
 //         check how many element with class "test" there are on page (5)
+        assertEquals(5, driver.findElements(By.className("test")).size());
 //         check that value of second button is "This is also a button"
+        assertEquals("This is also a button", driver.findElement(By.id("buttonId")).getAttribute("value"));
     }
 
     @Test
     public void assertTrueTask() throws Exception {
-        assertTrue("Custom message", driver.findElement(By.name("randomButton2")).getAttribute("value").equalsIgnoreCase("this is Also a Button"));
-//         TODO:
 //         check that it is True that value of second button is
 //         "this is Also a Button" if you ignore Caps Locks
 //         fail with custom error message:
+        assertTrue("Custom message", driver.findElement(By.name("randomButton2")).getAttribute("value").equalsIgnoreCase("this is Also a Button"));
     }
 
     @Test
     public void assertFalseTask() throws Exception {
-        assertFalse(driver.findElement(By.name("randomButton2")).getAttribute("value").equals("This is a button"));
-//         TODO:
 //        check that it is False that value of second button is "This is a button"
+        assertFalse(driver.findElement(By.name("randomButton2")).getAttribute("value").equals("This is a button"));
     }
 
     @Test
     public void failTask() throws Exception {
-        List<WebElement> elements = driver.findElements(By.className("test"));
-        for (WebElement element : elements) {
-            assertFalse(element.getText().contains("number: \"190"));
-        }
-//        TODO:
 //        check that none of items with class "test"
+        List<WebElement> elements = driver.findElements(By.className("test"));
 //        contain number 190
+        for (WebElement element : elements) {
+            assertFalse(element.getText().contains("190"));
+        }
     }
 }
--- src/selenium/sample/Sample5Task.java
+++ src/selenium/sample/Sample5Task.java
@@ -33,22 +33,31 @@ public class Sample5Task {
 
     @Test
     public void goToAlertedPageViaButton() throws Exception {
-//         TODO:
 //        click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
 //        switch to alert
+        Alert alert = driver.switchTo().alert();
 //        click ok
+        alert.accept();
 //        switch to second alert
+        Alert alert2 = driver.switchTo().alert();
 //        verify alert text
+        assertEquals("Booooooooo!", alert2.getText());
 //        click ok on second alert
+        alert2.accept();
 //        verify that the correct page is opened
+        assertEquals("https://kristinek.github.io/site/examples/alerted_page", driver.getCurrentUrl());
     }
 
     @Test
     public void doNotGoToAlertedPageViaButton() throws Exception {
-//         TODO:
 //        click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
 //        switch to alert
+        Alert alert = driver.switchTo().alert();
 //        click cancel
+        alert.dismiss();
 //        verify the text on page
+        assertEquals("So you desided to say? Good!", driver.findElement(By.id("textForAlerts")).getText());
     }
 }
