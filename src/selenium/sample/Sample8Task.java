7f263d517e0149360095b9980298b009137d8a5c
laura.junga <laura.junga@accenture.com>
2019-04-02 15:04:31 +0300

    sample 8 test

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -34,9 +34,13 @@ public class Sample8Task {
 
     @Test
     public void styleChecks() throws Exception {
-//         TODO:
 //        check the background of top 2 sections
-//        rgba(255, 221, 221, 1);
+        assertEquals("rgba(255, 221, 221, 1)",
+                driver.findElement(By.cssSelector(".w3-pale-red")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 204, 1)",
+                driver.findElement(By.cssSelector(".w3-pale-yellow")).getCssValue("background-color"));
 //        check h1 element font-size 64px
+        assertEquals("64px",
+                driver.findElement(By.cssSelector("h1")).getCssValue("font-size"));
     }
 }
