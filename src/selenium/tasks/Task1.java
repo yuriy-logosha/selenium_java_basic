079625b2cd341a7230651cdd4c4fee07f55dda95
Irina <i.ivanova2@inbox.lv>
2019-08-20 14:04:38 +0300

    Completed Task1

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,8 +3,14 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.omg.PortableInterceptor.ServerRequestInfo;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class Task1 {
     WebDriver driver;
@@ -27,12 +33,33 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+        String expectedError = "Please enter a number";
+        String enter = "Hi";
+        WebElement textArea = driver.findElement(By.id("numb"));
+        WebElement errorArea = driver.findElement(By.id("ch1_error"));
+        WebElement button = driver.findElement(By.className("w3-btn"));
+
+        textArea.sendKeys(enter);
+        button.click();
+
+        assertTrue(errorArea.getText().equals(expectedError));
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+        String expectedError = "Number is too small";
+        String enter = "15";
+
+        WebElement textArea = driver.findElement(By.id("numb"));
+        WebElement errorArea = driver.findElement(By.id("ch1_error"));
+        WebElement button = driver.findElement(By.className("w3-btn"));
+
+        textArea.sendKeys((enter));
+        button.click();
+
+        assertTrue(errorArea.getText().equals(expectedError));
     }
 
     @Test
@@ -41,6 +68,17 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        String expectedError = "Number is too big";
+        String enter = "115";
+
+        WebElement textArea = driver.findElement(By.id("numb"));
+        WebElement errorArea = driver.findElement(By.id("ch1_error"));
+        WebElement button = driver.findElement(By.className("w3-btn"));
+
+        textArea.sendKeys((enter));
+        button.click();
+
+        assertTrue(errorArea.getText().equals(expectedError));
     }
 
     @Test
@@ -48,6 +86,20 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+        String enter = "64";
+        // Square root of 64 is 8.00
+        String expectedError = "Square root of 64 is 8.00";
+
+        WebElement textArea = driver.findElement(By.id("numb"));
+        WebElement button = driver.findElement(By.className("w3-btn"));
+
+        textArea.sendKeys((enter));
+        button.click();
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        alert.accept();
+
+        assertTrue(alertText.equals(expectedError));
     }
 
     @Test
@@ -55,5 +107,19 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
+        String enter = "60";
+        // Square root of 60 is 7.75
+        String expectedError = "Square root of 60 is 7.75";
+
+        WebElement textArea = driver.findElement(By.id("numb"));
+        WebElement button = driver.findElement(By.className("w3-btn"));
+
+        textArea.sendKeys((enter));
+        button.click();
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        alert.accept();
+
+        assertTrue(alertText.equals(expectedError));
     }
 }
