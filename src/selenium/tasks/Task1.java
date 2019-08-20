04f933427d7c9582dead43c42d14e7d6c993c8a9
JanisKlavins <janis.klavins24@gmail.com>
2019-08-20 15:54:36 +0300

    My commit

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,8 +3,11 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.*;
 
 public class Task1 {
     WebDriver driver;
@@ -24,19 +27,37 @@ public class Task1 {
     }
 
     @Test
-    public void errorOnText() {
-//        TODO
+    public void errorOnText()  {
+
+        driver.findElement(By.id("numb")).sendKeys("abc");
+
+        driver.findElement(By.tagName("button")).click();
+
+        assertTrue(driver.findElement(By.id("ch1_error")).isDisplayed());
+
 //        enter a text instead of a number, check that correct error is seen
     }
 
     @Test
     public void errorOnNumberTooSmall() {
-//        TODO
+
+        driver.findElement(By.id("numb")).sendKeys("1");
+
+        driver.findElement(By.tagName("button")).click();
+
+        assertTrue(driver.findElement(By.id("ch1_error")).isDisplayed());
+
 //        enter number which is too small (below 50), check that correct error is seen
     }
 
     @Test
     public void errorOnNumberTooBig() {
+
+        driver.findElement(By.id("numb")).sendKeys("101");
+
+        driver.findElement(By.tagName("button")).click();
+
+        assertTrue(driver.findElement(By.id("ch1_error")).isDisplayed());
 
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
@@ -45,6 +66,13 @@ public class Task1 {
 
     @Test
     public void correctSquareRootWithoutRemainder() {
+        driver.findElement(By.id("numb")).sendKeys("64");
+
+        driver.findElement(By.tagName("button")).click();
+
+        Alert alert = driver.switchTo().alert();
+        assertTrue(alert.getText().contains("8"));
+
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
@@ -52,6 +80,15 @@ public class Task1 {
 
     @Test
     public void correctSquareRootWithRemainder() {
+        driver.findElement(By.id("numb")).sendKeys("55");
+
+        driver.findElement(By.tagName("button")).click();
+
+        Alert alert = driver.switchTo().alert();
+        assertTrue(alert.getText().contains("7.42"));
+
+
+
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
