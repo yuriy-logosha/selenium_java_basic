ccec3b8880dfd804403cb56e58525103c01b5309
AnastasijaG <gutmaneaj@gmail.com>
2019-08-20 13:06:31 +0300

    Task1 completed

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,8 +3,14 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.sleep;
+import static org.junit.Assert.assertEquals;
 
 public class Task1 {
     WebDriver driver;
@@ -24,23 +30,61 @@ public class Task1 {
     }
 
     @Test
-    public void errorOnText() {
+    public void errorOnText() throws InterruptedException {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+
+            WebElement textArea = driver.findElement(By.id("numb"));
+
+            String newTextOne = "Zina";
+            textArea.sendKeys(newTextOne);
+
+            driver.findElement(By.className("w3-btn")).click();
+
+        System.out.println(driver.findElement(By.id("ch1_error")).getText());
+        assertEquals("Please enter a number", driver.findElement(By.id("ch1_error")).getText());
+        sleep(1000);
+
+//        Alert alert = driver.switchTo().alert();
+//        assertEquals("Please enter a number", alert.getText());
+
     }
 
     @Test
-    public void errorOnNumberTooSmall() {
+    public void errorOnNumberTooSmall() throws InterruptedException {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+        driver.findElement(By.id("numb")).sendKeys("47");
+        driver.findElement(By.className("w3-btn")).click();
+        assertEquals("Number is too small", driver.findElement(By.id("ch1_error")).getText());
+
+        System.out.println(driver.findElement(By.id("ch1_error")).getText());
+        sleep(1000);
     }
 
     @Test
-    public void errorOnNumberTooBig() {
+    public void errorOnNumberTooBig() throws InterruptedException {
+
+        WebElement textArea = driver.findElement(By.id("numb"));
+
+
 
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        driver.findElement(By.id("numb")).sendKeys("120");
+        driver.findElement(By.className("w3-btn")).click();
+        assertEquals("Number is too big", driver.findElement(By.id("ch1_error")).getText());
+
+        System.out.println(driver.findElement(By.id("ch1_error")).getText());
+        sleep(1000);
+
+        textArea.clear();
+
+        driver.findElement(By.id("numb")).sendKeys("666");
+        driver.findElement(By.className("w3-btn")).click();
+        assertEquals("", driver.findElement(By.id("ch1_error")).getText());
+        sleep(10000);
     }
 
     @Test
@@ -48,12 +92,32 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+        driver.findElement(By.id("numb")).sendKeys("81");
+        driver.findElement(By.className("w3-btn")).click();
+
+        Alert alert = driver.switchTo().alert();
+        alert.getText();
+        assertEquals("Square root of 81 is 9.00", alert.getText());
     }
 
     @Test
-    public void correctSquareRootWithRemainder() {
+    public void correctSquareRootWithRemainder() throws InterruptedException {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
+        driver.findElement(By.id("numb")).sendKeys("87");
+        driver.findElement(By.className("w3-btn")).click();
+
+        Alert alert = driver.switchTo().alert();
+        alert.getText();
+        assertEquals("Square root of 87 is 9.33", alert.getText());
+
+        alert.accept();
+        assertEquals("", driver.findElement(By.id("ch1_error")).getText());
+
+//        System.out.println(driver.alert.getText));
+        sleep(1000);
+
+
     }
 }
