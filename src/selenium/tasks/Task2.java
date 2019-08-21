d947529ff39b4bc15c9abc7e08f80b720ae58766
Irina Bistrova <irina.bi@apollo.lv>
2019-08-21 17:59:30 +0300

    tasks2

--- src/selenium/tasks/Task2.java
+++ src/selenium/tasks/Task2.java
@@ -3,8 +3,16 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import javax.xml.soap.Text;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class Task2 {
     WebDriver driver;
@@ -14,7 +22,7 @@ public class Task2 {
         String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://kristinek.github.io/sitetasks/provide_feedback");
+        driver.get("https://kristinek.github.io/site/tasks/provide_feedback");
     }
 
     @After
@@ -29,7 +37,27 @@ public class Task2 {
 //         "Don't know" is selected in "Genre"
 //         "Choose your option" in "How do you like us?"
 //         check that the button send is blue with white letters
-    }
+        assertTrue(driver.findElement(By.id("fb_name")).getText().isEmpty());
+        assertTrue(driver.findElement(By.id("fb_age")).getText().isEmpty());
+        assertFalse(driver.findElement(By.xpath("//input[@name='language' and @value='English']")).isSelected());
+        assertFalse(driver.findElement(By.xpath("//input[@name='language' and @value='French']")).isSelected());
+        assertFalse(driver.findElement(By.xpath("//input[@name='language' and @value='Spanish']")).isSelected());
+        assertFalse(driver.findElement(By.xpath("//input[@name='language' and @value='Chinese']")).isSelected());
+
+
+        List<WebElement> allElementsWithClass = driver.findElements(By.className("w3-radio"));
+        WebElement thirdButton = allElementsWithClass.get(2);
+        assertTrue(thirdButton.isSelected());
+
+        Select select = new Select(driver.findElement(By.id("like_us")));
+       select.getFirstSelectedOption();
+
+        WebElement sendButton = driver.findElement(By.className("w3-btn-block"));
+        assertEquals("rgba(255, 255, 255, 1)", sendButton.getCssValue("color"));
+        assertEquals("rgba(33, 150, 243, 1)", sendButton.getCssValue("background-color"));
+
+
+   }
 
     @Test
     public void emptyFeedbackPage() throws Exception {
@@ -38,6 +66,25 @@ public class Task2 {
 //         check fields are empty or null
 //         check button colors
 //         (green with white letter and red with white letters)
+        driver.findElement(By.className("w3-btn-block")).click();
+        assertTrue(driver.findElement(By.id("name")).getText().isEmpty());
+        assertTrue(driver.findElement(By.id("age")).getText().isEmpty());
+        assertTrue(driver.findElement(By.id("language")).getText().isEmpty());
+        assertTrue(driver.findElement(By.id("comment")).getText().isEmpty());
+        WebElement yourGenre = driver.findElement(By.id("gender"));
+        assertEquals("null",yourGenre.getText());
+        WebElement yourOption = driver.findElement(By.id("option"));
+        assertEquals("null", yourOption.getText());
+
+        WebElement buttonYes = driver.findElement(By.xpath("//*[@id=\"fb_thx\"]/div/div[2]/button[1]"));
+        assertEquals("rgba(255, 255, 255, 1)", buttonYes.getCssValue("color")) ;
+        assertEquals("rgba(76, 175, 80, 1)", buttonYes.getCssValue("background-color"));
+
+        WebElement buttonNo = driver.findElement(By.xpath("//*[@id=\"fb_thx\"]/div/div[2]/button[2]"));
+        assertEquals("rgba(255, 255, 255, 1)", buttonNo.getCssValue("color"));
+        assertEquals("rgba(244, 67, 54, 1)", buttonNo.getCssValue("background-color"));
+
+
     }
 
     @Test
@@ -47,6 +94,10 @@ public class Task2 {
 //         check fields are filled correctly
 //         check button colors
 //         (green with white letter and red with white letters)
+        WebElement name = driver.findElement(By.id("fb_name")).sendKeys();
+        String newName = "Irina";
+        name.sendKeys(newName);
+        assertEquals(
     }
 
     @Test
